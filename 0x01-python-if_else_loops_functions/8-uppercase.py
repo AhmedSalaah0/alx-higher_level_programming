074e3afeaa240802cba51e9b6ea08af97617b81f
Ahmed Salah <ahmed.salah200555@gmail.com>
2024-03-14 04:23:19 -0700

    UpperCase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     res = ""
     for i in range(len(str)):
-        if(ord(str[i]) >= 97 and ord(str[i]) <= 123):
-          res += chr(ord(str[i]) - 32)
+        if ord(str[i]) >= 97 and ord(str[i]) <= 123:
+            res += chr(ord(str[i]) - 32)
         else:
             res += str[i]
     print("{}".format(res))
