279d0109688fa89e4f35456e9db4ebf8e9c1ff77
Ahmed Salah <ahmed.salah200555@gmail.com>
2024-03-14 05:13:47 -0700

    Advanced One

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 char = 122
 for i in range(26):
-        print("{}".format(chr(char if i % 2 == 0 else char - 32)), end="")
-        char -= 1
+    print("{}".format(chr(char if i % 2 == 0 else char - 32)), end="")
+    char -= 1
