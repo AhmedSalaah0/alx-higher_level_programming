25eadd542a4870a2d43672e71ffd01f31bb6c5f2
Ahmed Salah <ahmed.salah200555@gmail.com>
2024-03-17 05:55:21 -0700

    2

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    import calculator_1 as s
+    a = 10
+    b = 5
+    print("{}".format(s.add(a, b)))
+    print("{}".format(s.sub(a, b)))
+    print("{}".format(s.mul(a, b)))
+    print("{}".format(s.div(a, b)))
