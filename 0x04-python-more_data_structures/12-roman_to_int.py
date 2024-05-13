2e2d3713249f2cc74920a6c8e7cbcaccf97e84bb
ahmed <ahmed.salah200555@gmail.com>
2024-05-13 15:24:23 +0300

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev_value = 0
+    for char in roman_string:
+        value = roman[char]
+        if value > prev_value:
+            total += value - 2 * prev_value
+        else:
+            total += value
+        prev_value = value
+    return total
